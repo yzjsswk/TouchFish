49de1daf339ec88f02910b5c4dc40fc99940cf7f
yzjsswk <yzjsswk@163.com>
2024-03-26 16:20:43 +0800

    Initial Commit

---
+++ TouchFish/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TouchFish
+//
+//  Created by 叶志杰 on 2024/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TouchFish/TouchFishApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchFishApp.swift
+//  TouchFish
+//
+//  Created by 叶志杰 on 2024/3/26.
+//
+
+import SwiftUI
+
+@main
+struct TouchFishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
