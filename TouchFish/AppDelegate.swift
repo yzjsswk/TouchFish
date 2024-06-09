e5f7187518d6a7314e4c3c552d949bd85e1e494b
yzjsswk <yzjsswk@163.com>
2024-06-09 22:33:53 +0800

    remove some code not required

--- TouchFish/AppDelegate.swift
+++ TouchFish/AppDelegate.swift
@@ -6,14 +6,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
         TouchFishApp.start()
-        let options: NSDictionary = [kAXTrustedCheckOptionPrompt.takeUnretainedValue() as NSString: true]
-        let accessEnabled = AXIsProcessTrustedWithOptions(options)
-
-        if !accessEnabled {
-            print("Access Not Enabled")
-        } else {
-            print("Access Enabled")
-        }
 	}
 
     
